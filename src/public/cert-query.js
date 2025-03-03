f2c5c2a2c41c187bdd75da50186f2736f871158f
halildundar <halildundar.eee@gmail.com>
2025-03-04 01:10:24 +0300

    all30

--- src/public/cert-query.js
+++ src/public/cert-query.js
@@ -1,13 +1,12 @@
 import "./cert-query.scss";
 
-$(document).ready(function () {
+$(document).ready(async function () {
   $("#search-btn").click(async function (e) {
     e.preventDefault();
     if (!$("#search-txt").val()) {
       //Bu alan Boş olamaz
       return;
     }
-    console.log($("#search-txt").val())
     const response = await $.ajax({
       type: "POST",
       url: "/cert-query",
@@ -56,7 +55,47 @@ $(document).ready(function () {
   // window.location.search = '';
   window.history.replaceState(null, "", window.location.pathname);
   if (!!CertQ) {
-    $("#search-txt").val(CertQ);
-    $("#search-btn").click();
+    const response = await $.ajax({
+      type: "POST",
+      url: "/cert-query",
+      data: JSON.stringify({
+        cert_id:CertQ
+      }),
+      dataType: "json",
+      contentType:"application/json"
+    });
+    if (!!response) {
+      const { msg } = response;
+      $("#docVer").removeClass("hidden");
+      $("#articArea").addClass("hidden");
+      if (msg == "Finded") {
+        $("#docVer table").removeClass("hidden");
+        $("#noCert").addClass("hidden");
+        $("#docVer table tbody").html("");
+        delete response["msg"];
+        $.map(response, function (val, key) {
+          if (key !== "id") {
+            let Keys = key.split("_").map((item) => {
+              return (
+                String(item).charAt(0).toUpperCase() + String(item).slice(1)
+              );
+            });
+            newKey = "";
+            for (let I = 0; I < Keys.length; I++) {
+              newKey += I > 0 ? " " + Keys[I] : Keys[I];
+            }
+            $("#docVer table tbody").append(
+              $("<tr>")
+                .append($("<td>", { class:'bg-black/20',text: `${newKey}` }))
+                .append($("<td>", { text: `${val}` }))
+            );
+          }
+        });
+      } else {
+        $("#docVer table").addClass("hidden");
+        $("#noCert").removeClass("hidden");
+      }
+    }
+  
   }
 });
